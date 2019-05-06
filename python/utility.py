c72aad777d529ab44e948e280361a3e0e6e6c38d
Clemens Hagenbuchner <>
2019-05-06 15:42:34 +0200

    Create utility.py

---
+++ python/utility.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Plots:
+    # will be released after the final deadline
+    pass
+    
+class FPlots:
+    pass
+    
+class Transforms:
+    pass
+    
+class Signal: 
+    pass
